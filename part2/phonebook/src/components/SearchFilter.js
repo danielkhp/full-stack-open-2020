8f37515f8ade4130042b945dbc287cdcba3e9c1e
Daniel <danielkhp@gmail.com>
2020-12-20 09:47:58 -0600

    Complete exercise 2.11

--- part2/phonebook/src/components/SearchFilter.js
+++ part2/phonebook/src/components/SearchFilter.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
 const SearchFilter = ({ filter, onChange }) => (
-    <>
-      filter by name: <input
-                          value={filter}
-                          onChange={onChange}
-                        />
-    </>
+  <>
+    filter by name: 
+    <input
+      value={filter}
+      onChange={onChange}
+    />
+  </>
 )
 
 export default SearchFilter
